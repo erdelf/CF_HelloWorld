9002e61b04858e2f2988df3323f3b847a9743fe9
Simon Latusek <Simon.Latusek@gmail.com>
2025-07-20 05:12:00 +0200

    initial drawing test

---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <cute.h>
+#include <cstdio>
+
+struct Circle
+{
+    float radius, thickness;
+	CF_Color color = cf_color_white();
+};
+
+struct Game
+{
+    dyna Circle* circles;
+};
+
+static void init(void* udata)
+{
+    Game* game = (Game*)udata;
+
+    dyna Circle* circles = game->circles;
+
+    cf_array_clear(circles);
+    for (int i = 0; i < 10; ++i)
+    {
+        Circle circle ;
+        circle.radius = 20 + i * 10;
+        circle.thickness = 5;
+        cf_array_push(game->circles, circle);
+	}
+}
+
+static void fixedUpdate(void* udata)
+{
+    Game* game = (Game*) udata;
+
+	dyna Circle* circles = game->circles;
+
+    for (int i = 0; i < cf_array_count(circles); ++i)
+    {
+        Circle* circle = circles + i;
+		circle->thickness += cf_sin_in_out(CF_TICKS * 2.0f + i) * 2 - 1;
+    }
+    /*
+    update_blocks(game);
+    update_stars(game);
+    update_players(game);
+    update_star_particles(game);
+
+    if (cf_key_just_pressed(CF_KEY_R))
+    {
+        load_level(game);
+    }*/
+}
+
+
+static void update(void* udata)
+{
+    Game* game = (Game*)udata;
+
+    dyna Circle* circles = game->circles;
+
+    for (int i = 0; i < cf_array_count(circles); ++i)
+    {
+        Circle* circle = circles + i;
+        cf_draw_push_color(circle->color);
+		cf_draw_circle2(V2(0, 0), circle->radius, circle->thickness);
+		cf_draw_pop_color();
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    CF_Result result = cf_make_app("MainWindow", 0, 0, 0, 640, 480, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]);
+    if (cf_is_error(result))
+        return -1;
+
+    cf_set_target_framerate(60);
+    cf_set_fixed_timestep(30);
+
+	Game game = {};
+
+    cf_set_update_udata(&game);
+
+    init(&game);
+
+    while (cf_app_is_running())
+    {
+        cf_app_update(fixedUpdate);
+
+        update(&game);
+
+        cf_app_draw_onto_screen(true);
+    }
+
+    cf_destroy_app();
+
+    return 0;
+}
