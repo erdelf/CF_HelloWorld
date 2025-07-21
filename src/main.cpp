86d16afc9088c48ea57cd732549bc8a6fd35eaf1
Simon Latusek <Simon.Latusek@gmail.com>
2025-07-21 02:57:22 +0200

    naming sweep

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,47 @@
 #include <cute.h>
 #include <cmath>
-#include <cstdio>
 #include <ctime>
 #include <memory>
-#include <string>
 #include <vector>
+#include <thread>
 
-namespace HelloWorld
+namespace hello_world
 {
-    constexpr int width = 1920;
-    constexpr int height = 1080;
+    constexpr int kWidth = 1920;
+    constexpr int kHeight = 1080;
 
-    constexpr int widthHalf = width / 2;
-    constexpr int heightHalf = height / 2;
+    constexpr int kWidthHalf = kWidth / 2;
+    constexpr int kHeightHalf = kHeight / 2;
 
     struct BasicObject
     {
         virtual ~BasicObject() = default;
         CF_ShapeType collisionShapeType = CF_SHAPE_TYPE_NONE;
-        std::unique_ptr<void> collisionShape = nullptr;
+        void* collisionShape = nullptr;
 
         CF_V2 position;
         CF_V2 positionDraw;
 
         bool movingOutOfWall = false;
 
-        void set_position(const CF_V2& new_pos)
+        void setPosition(const CF_V2& newPos)
         {
-            this->position = new_pos;
-            this->positionDraw = new_pos;
+            position = newPos;
+            positionDraw = newPos;
         }
-
 
         CF_V2 velocity;
         bool velocityAdjusted;
 
-        void Draw()
+        void draw()
         {
             cf_draw_push();
             cf_draw_translate_v2(positionDraw);
-            DrawInt();
+            drawInt();
             cf_draw_pop();
         }
 
-        virtual void DrawInt()
+        virtual void drawInt()
         {
         }
 
@@ -52,27 +50,27 @@ namespace HelloWorld
             positionDraw = positionDraw + velocity * CF_DELTA_TIME;
         }
 
-        virtual void fixed_update()
+        virtual void fixedUpdate()
         {
             velocityAdjusted = false;
 
             const CF_V2 pos = position + velocity * CF_DELTA_TIME_FIXED;
-            set_position(pos);
+            setPosition(pos);
 
             if (movingOutOfWall)
             {
                 bool done = false;
 
-                if (pos.x < -widthHalf && velocity.x < 0)
+                if (pos.x < -kWidthHalf && velocity.x < 0)
                     velocity.x = abs(velocity.x);
-                else if (pos.x > widthHalf && velocity.x > 0)
+                else if (pos.x > kWidthHalf && velocity.x > 0)
                     velocity.x = abs(velocity.x) * -1;
                 else
                     done = true;
 
-                if (pos.y < -heightHalf && velocity.y < 0)
+                if (pos.y < -kHeightHalf && velocity.y < 0)
                     velocity.y = abs(velocity.y);
-                else if (pos.y > heightHalf && velocity.y > 0)
+                else if (pos.y > kHeightHalf && velocity.y > 0)
                     velocity.y = abs(velocity.y) * -1;
                 else if (done)
                 {
@@ -82,27 +80,26 @@ namespace HelloWorld
                 return;
             }
 
-
             // Bounce off walls
-            if (pos.x < -widthHalf || pos.x > widthHalf)
+            if (pos.x < -kWidthHalf || pos.x > kWidthHalf)
             {
                 velocity.x = -velocity.x;
                 movingOutOfWall = true;
             }
 
-            if (pos.y < -heightHalf || pos.y > heightHalf)
+            if (pos.y < -kHeightHalf || pos.y > kHeightHalf)
             {
                 velocity.y = -velocity.y;
                 movingOutOfWall = true;
             }
         }
 
-        virtual void CollisionTestWith(BasicObject* other)
+        virtual void collisionTestWith(BasicObject* other)
         {
             if (other == nullptr || other == this || collisionShapeType == CF_SHAPE_TYPE_NONE || other->collisionShapeType == CF_SHAPE_TYPE_NONE || (velocityAdjusted && other->velocityAdjusted))
                 return;
 
-            if (CollisionTestWithInt(other))
+            if (collisionTestWithInt(other))
             {
                 CF_V2 velNew = position - other->position;
                 velNew = velNew / cf_len(velNew);
@@ -115,54 +112,22 @@ namespace HelloWorld
 				velocityAdjusted = true;
                 other->velocityAdjusted = true;
             }
-
-
-
-            /**
-             *  manual works but currently hardcoded for circles
-            if (cf_len((position - other->position)) < (((CF_Circle*)(collisionShape))->r + ((CF_Circle*)(other->collisionShape))->r))
-            {
-                printf("collision detected\n");
-            }
-            */
-
-            /**
-                Seemingly none of the cf collision functions work, so this is commented out for now.
-
-            CF_Transform* transform = new CF_Transform();
-            transform->r = cf_sincos();
-            transform->p = position;
-
-            CF_Transform* transformOther = new CF_Transform();
-            transformOther->r = cf_sincos();
-            transformOther->p = other->position;
-
-            if (cf_circle_to_circle(*((CF_Circle*)(collisionShape)), *((CF_Circle*)(other->collisionShape))))
-                printf("circle to circle collision detected\n");
-
-            if (cf_collided(collisionShape, transform, collisionShapeType, other->collisionShape, transformOther, other->collisionShapeType))
-                printf("collided\n");
-            */
         }
 
-        virtual bool CollisionTestWithInt(BasicObject* other)
+        virtual bool collisionTestWithInt(BasicObject* other)
         {
-            //Hardcoded for circles atm. Best case would be to check all different collision shapes
-            const float combinedShapeDistance = ((CF_Circle*)collisionShape.get())->r + ((CF_Circle*)other->collisionShape.get())->r;
+            const float combinedShapeDistance = ((CF_Circle*)collisionShape)->r + ((CF_Circle*)other->collisionShape)->r;
             const float distance = cf_len_sq(position - other->position);
-            if (distance < combinedShapeDistance * combinedShapeDistance)
-                return true;
-            return false;
+            return distance < combinedShapeDistance * combinedShapeDistance;
         }
     };
-
 
     struct Circle
     {
         float radius, thickness;
         CF_Color color = cf_color_white();
 
-        void Draw() const
+        void draw() const
         {
             cf_draw_push_color(color);
             cf_draw_circle2(V2(0, 0), radius, thickness);
@@ -177,11 +142,8 @@ namespace HelloWorld
         BouncingCircle()
         {
             collisionShapeType = CF_SHAPE_TYPE_CIRCLE;
-
             circles = {};
-
             const int circleCount = rand() % 2 + 1;
-
             float highestRadius = 0.f;
 
             for (int c = 0; c < circleCount; c++)
@@ -189,22 +151,19 @@ namespace HelloWorld
                 Circle circle;
                 circle.radius = 1.f + static_cast<float>(c) * 10.f;
                 circle.thickness = 5;
-
                 highestRadius = circle.radius + 10;
-
 
                 const float h = static_cast<float>(rand()) / RAND_MAX; // Hue: [0, 1]
                 const float s = 0.5f + static_cast<float>(rand()) / (1 / .5f * RAND_MAX); // Saturation: [0.5, 1]
                 const float v = 0.7f + static_cast<float>(rand()) / (1 / .3f * RAND_MAX); // Value: [0.7, 1]
 
                 circle.color = cf_hsv_to_rgb(CF_Color(h, s, v, 1.));
-
                 cf_array_push(circles, circle);
             }
-            position = V2(rand() % width - widthHalf, rand() % height - heightHalf);
-            velocity = V2(rand() % (width / 2) * (rand() % 2 == 1 ? 1 : -1), rand() % (height / 2) * (rand() % 2 == 1 ? 1 : -1));
-
-            collisionShape = std::make_unique<CF_Circle>(CF_Circle{ position, highestRadius });
+            position = V2(rand() % kWidth - kWidthHalf, rand() % kHeight - kHeightHalf);
+            velocity = V2(rand() % (kWidth / 2) * (rand() % 2 == 1 ? 1 : -1), rand() % (kHeight / 2) * (rand() % 2 == 1 ? 1 : -1));
+            CF_Circle* cf_circle = new CF_Circle { position, highestRadius };
+            collisionShape = cf_circle;
         }
 
         ~BouncingCircle() override
@@ -213,10 +172,10 @@ namespace HelloWorld
             circles = nullptr;
         }
 
-        void DrawInt() override
+        void drawInt() override
         {
             for (int c = 0; c < cf_array_count(circles); ++c)
-                (circles + c)->Draw();
+                (circles + c)->draw();
         }
     };
 
@@ -233,16 +192,13 @@ namespace HelloWorld
     static void init(void* udata)
     {
         Game* game = (Game*)udata;
-
         std::vector<std::unique_ptr<BasicObject>> objects;
-
         for (int i = 0; i < 1000; ++i)
-	        objects.push_back(std::make_unique<BouncingCircle>());
-
+            objects.push_back(std::make_unique<BouncingCircle>());
         game->objects = std::move(objects);
     }
 
-    static void fixed_update(void* udata)
+    static void fixedUpdate(void* udata)
     {
         const Game* game = (Game*)udata;
         const auto& objects = game->objects;
@@ -254,7 +210,9 @@ namespace HelloWorld
 
 
         for (const auto& obj : objects)
-            obj->fixed_update();
+            obj->fixedUpdate();
+
+        
         auto collision_worker = [&](const size_t start, const size_t end) {
             for (size_t i = start; i < end; ++i) {
                 for (size_t j = 0; j < numObjects; ++j) {
@@ -265,29 +223,36 @@ namespace HelloWorld
             }
         };
 
-        for (const auto& objA : objects)
-            for (const auto& objB : objects)
-                objA->CollisionTestWith(objB.get());
-    }
+        const size_t chunk = (numObjects + numThreads - 1) / numThreads;
+        for (unsigned t = 0; t < numThreads; ++t) {
+            size_t start = t * chunk;
+            size_t end = std::min(start + chunk, numObjects);
+            if (start < end) {
+                threads.emplace_back(collision_worker, start, end);
+            }
+        }
 
+        for (auto& th : threads) {
+            th.join();
+        }
+    }
 
     static void update(void* udata)
     {
         const Game* game = (Game*)udata;
-
         const auto& objects = game->objects;
 
         for (const auto& obj : objects)
             obj->update();
 
         for (const auto& obj : objects)
-            obj->Draw();
+            obj->draw();
     }
 }
 
 int main(int argc, char* argv[])
 {
-    if (const CF_Result result = cf_make_app("MainWindow", 0, 0, 0, HelloWorld::width, HelloWorld::height, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]); cf_is_error(result))
+    if (const CF_Result result = cf_make_app("MainWindow", 0, 0, 0, hello_world::kWidth, hello_world::kHeight, CF_APP_OPTIONS_WINDOW_POS_CENTERED_BIT, argv[0]); cf_is_error(result))
         return -1;
 
     cf_set_target_framerate(60);
@@ -296,14 +261,14 @@ int main(int argc, char* argv[])
 
     srand((unsigned)time(nullptr));
 
-	HelloWorld::Game game;
+	hello_world::Game game;
 
     cf_set_update_udata(&game);
     init(&game);
 
     while (cf_app_is_running())
     {
-        cf_app_update(HelloWorld::fixed_update);
+        cf_app_update(hello_world::fixedUpdate);
 
         update(&game);
 
